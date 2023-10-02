cd6747c0a387e426feddef083b32326842b7e984
danse <dans@curve.tech>
2023-10-02 17:51:19 +0300

    change from int to str in load game. q and s doesn't work

--- Live.py
+++ Live.py
@@ -1,7 +1,10 @@
-def welcome(name):
-    hello = (f"Hello {name} and welcome to the World of Games (WoG).\n"
+from pyfiglet import Figlet
+
+
+def welcome(username):
+    hello = (f"Hello {username} and welcome to the World of Games (WoG).\n"
              f"Here you can find many cool games to play.")
-    return hello
+    return hello, username
 
 
 def load_game():
@@ -11,15 +14,19 @@ def load_game():
     is_valid_input = False
 
     while is_valid_input is False:
-        game_chooser = input(
+        f = Figlet(font='slant')
+        print(f.renderText('World of Games'), end='')
+        # currently only
+        game_chooser = str(input(
+
             f"Please choose a game to play: (To choose type the number of the game and press enter)\n"
             f"1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back\n"
             f"2. Guess Game - guess a number and see if you chose like the computer\n"
             f"3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n"
-            f"q. To quit\n")
+            f"s. Show Score Board\n"
+            f"q. Quit\n"))
         try:
-            game_chooser = int(game_chooser)
-            if isinstance(int(game_chooser), int) and 1 <= int(game_chooser) <= 3:
+            if str(game_chooser) in ["1", "2", "3", "s", "q"]:
                 is_valid_input = True
             else:
                 print(too_much)
@@ -44,23 +51,27 @@ def load_game():
 
 
 def open_game(game_chooser, difficulty):
-    if game_chooser == 1:
+    if game_chooser == "1":
         from MemoryGame import play
         return play(difficulty)
-    elif game_chooser == 2:
+    elif game_chooser == "2":
         from GuessGame import play
         return play(difficulty)
-    elif game_chooser == 3:
+    elif game_chooser == "3":
         from CurrencyRouletteGame import play
         return play(difficulty)
+    elif game_chooser == "s":
+        pass
     elif game_chooser == "q":
         print("Thank you for playing, see you next time!")
-        return
+        return None
 
 
 def main():
     while True:
         game_data = load_game()
+        if game_data is None:
+            break
         game_chooser = game_data["game_chooser"]
         difficulty = game_data["difficulty"]
         open_game(game_chooser, difficulty)
