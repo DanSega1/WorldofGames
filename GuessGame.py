3cda5fa0c12d9cda801559a5fdbb8e043b5eb3dc
danse <dans@curve.tech>
2023-10-03 17:02:04 +0300

    add score mechanism

--- GuessGame.py
+++ GuessGame.py
@@ -1,6 +1,5 @@
 import random
-# from Score import add_score_board
-
+from Score import Score
 
 def generate_number(difficulty):
     secret_number = random.randint(1, difficulty + 1)
@@ -19,18 +18,22 @@ def compare_results(secret_number, user_guess):
         return False
 
 
-def play(difficulty):
+def play(difficulty, username):
     secret_number = generate_number(difficulty)
     user_guess = get_guess_from_user(difficulty)
     if secret_number == user_guess:
+        game_name = "Guess Game"
+        score_manager = Score()
+        new_txt_score, new_csv_score = score_manager.add_score(game_name, difficulty, username)
         print("You won! The number was {}".format(secret_number))
-        # add_score_board()
+        print(f"Your global score is: {new_txt_score} pt")
+        print(f"Your score in {game_name} is: {new_csv_score} pt")
     else:
         print("You lost! The number was {}".format(secret_number))
 
     play_again = input("Do you want to play again? (y/n) ").lower()
     if play_again == "y":
-        play(difficulty)
+        play(difficulty, username)
     else:
         print("Thank you for playing, see you next time!")
         return
